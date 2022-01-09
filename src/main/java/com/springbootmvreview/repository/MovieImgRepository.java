fd631036bca05d9b7667d6d3821b2bd5a5a67752
kdd9512 <hyunwoo8805@gmail.com>
2022-01-09 23:21:11 +0900

    20210109 2nd commit. ** application.properties 파일은 보안상 업로드 하지 않을 것.
    영화 리뷰 사이트 제작(m:n(다대다) 관계).
    test 코드로 DB에 데이터 삽입 및 테스트.

--- src/main/java/com/springbootmvreview/repository/MovieImgRepository.java
+++ src/main/java/com/springbootmvreview/repository/MovieImgRepository.java
@@ -2,6 +2,8 @@ package com.springbootmvreview.repository;
 
 import com.springbootmvreview.entity.MovieImgEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MovieImgRepository extends JpaRepository<MovieImgEntity, Long> {
+
 }
--- src/main/java/com/springbootmvreview/repository/MovieRepository.java
+++ src/main/java/com/springbootmvreview/repository/MovieRepository.java
@@ -1,8 +1,25 @@
 package com.springbootmvreview.repository;
 
 import com.springbootmvreview.entity.MovieEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface MovieRepository extends JpaRepository<MovieEntity, Long> {
+
+    @Query("select m, mi, avg(coalesce(r.grade,0)), count(distinct r) from MovieEntity m " +
+            "left outer join MovieImgEntity mi on mi.movie = m " +
+            "left outer join ReviewEntity r on r.movie = m group by m")
+    Page<Object[]> getListPage(Pageable pageable); // 페이지 처리용 jpql
+
+    @Query("select m, mi, avg(coalesce(r.grade, 0)), count(r) " +
+            "from MovieEntity m left outer join MovieImgEntity mi on mi.movie = m " +
+            "left outer join ReviewEntity r on r.movie = m " +
+            "where m.mno = :mno group by mi")
+    List<Object[]> getMovieWithAll(Long mno); // 특정 영화 조회.
+
 
 }
