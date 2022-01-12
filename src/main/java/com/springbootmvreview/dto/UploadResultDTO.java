0eaaf893ceb35efadc38fe5f5e4483ea793e2e85
kdd9512 <hyunwoo8805@gmail.com>
2022-01-12 21:53:11 +0900

    20210111 3rd commit. ** application.properties 파일 업로드 (DB 정보 삭제하여 업로드. 이후 업로드 없을 것.)
    영화 리뷰 사이트 제작(m:n(다대다) 관계).
    local upload 작동 확인.
    image 파일만 업로드 가능하게.
    upload 직후 이미지 확인가능.
    remove 기능 추가.

--- src/main/java/com/springbootmvreview/dto/UploadResultDTO.java
+++ src/main/java/com/springbootmvreview/dto/UploadResultDTO.java
@@ -15,9 +15,18 @@ public class UploadResultDTO implements Serializable {
     private String uuid;
     private String folderPath;
 
-    public String getImageURL(){
+    public String getImageURL() {
         try {
             return URLEncoder.encode(folderPath + "/" + uuid + "_" + fileName, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public String getThumbnailURL() {
+        try {
+            return URLEncoder.encode(folderPath + "/s_" + uuid + "_" + fileName, "UTF-8");
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
